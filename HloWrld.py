b375fbb1206d095dce18a92f22e042f840044ab4
Elynor Moore <elynor.moore@wolfson.ox.ac.uk>
2019-10-24 16:18:53 +0100

    hello world code

---
+++ HloWrld.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+#Program written on 2013-05-13
+#Simple Hello World program
+#These are Python comments
+
+"""This is a multi-line comment
+in python"""
+
+print("Hello world!")
+
+"""Note that python implicitly inserts
+a new line character"""
